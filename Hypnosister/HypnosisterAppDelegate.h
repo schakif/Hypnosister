aa05ff8526f33b1dec7509d8cc99eb3807106df6
Steve Chakif <schakif@Steves-MacBook-Pro.local>
2014-02-06 16:07:35 -0500

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Steve Chakif on 2/6/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
